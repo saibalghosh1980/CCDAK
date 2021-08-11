e722564a2094266af9996bd4bda623453720710a
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2021-08-11 12:34:59 +0100

    NA

---
+++ simplekafka/src/test/java/com/cts/cdak/simplekafka/SimplekafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.cdak.simplekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplekafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- start-kafka.sh
+++ start-kafka.sh
@@ -1,4 +1,4 @@
-systemctl start confluent-zookeeper
-systemctl start confluent-kafka 
-journalctl -u confluent-zookeeper -f
-journalctl -u confluent-kafka -f
+sudo systemctl start confluent-zookeeper
+sudo systemctl start confluent-kafka 
+sudo journalctl -u confluent-zookeeper -f
+sudo journalctl -u confluent-kafka -f
